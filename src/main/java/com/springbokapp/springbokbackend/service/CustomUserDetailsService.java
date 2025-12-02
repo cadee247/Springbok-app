6b2a20462ad0a96d2713b41ba920d82e2986ffd0
Cadee Rousseau <rousseaucadee@gmail.com>
2025-12-02 17:58:13 +0200

    Update CustomUserDetailsService.java

--- src/main/java/com/springbokapp/springbokbackend/service/CustomUserDetailsService.java
+++ src/main/java/com/springbokapp/springbokbackend/service/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 
@@ -20,6 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
