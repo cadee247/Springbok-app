50b6fc331ea9b032c6c382e6e63fa4bcb5be25ea
cadee247 <rousseaucadee@gmail.com>
2025-12-02 18:40:56 +0200

    Update CustomUserDetailsService and EncodePassword util

--- src/main/java/com/springbokapp/springbokbackend/util/EncodePassword.java
+++ src/main/java/com/springbokapp/springbokbackend/util/EncodePassword.java
@@ -1,4 +1,10 @@
 package com.springbokapp.springbokbackend.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class EncodePassword {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("YourVeryUniquePassword123!"));
+    }
 }
